64e2529d97d2016da859ffb13dac2dc6741edb23
carledwardparing <paringedward0@gmail.com>
2020-01-31 13:44:00 +0800

    comment out style
    all apps style

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
 
 <style>
 
-.loginPage{
+/* .loginPage{
   margin-top:10%;
 }
 .size{
@@ -23,5 +23,5 @@ export default {
 #app{
   height: 200vh;
   background: #4d4d4d;
-}
+} */
 </style>
