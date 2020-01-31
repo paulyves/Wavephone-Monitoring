98734d7422503f94ab3474f979e9b3cccb7e4a38
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-01-31 10:17:07 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">   
+ <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+.loginPage{
+  margin-top:10%;
+}
+.size{
+  margin-right: 50%;
+}
+#app{
+  height: 200vh;
+  background: #4d4d4d;
+}
+</style>
