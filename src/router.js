30969c1d7f10317b76eb676e8e77687a901e1117
carledwardparing <paringedward0@gmail.com>
2020-01-31 13:47:39 +0800

    Update router.js
    fix
    login first before gridView

--- src/router.js
+++ src/router.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 //import Home from './views/Home.vue'
-import GridView from './views/GridView.vue'
 import Login from './views/login.vue'
+import GridView from './views/GridView.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 export default new VueRouter({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: GridView
-    },
-    {
-      path:'/login',
+      path:'/',
       name:'login',
       component: Login
+    },
+
+    {
+      path: '/gridView',
+      name: 'gridView',
+      component: GridView
     }
     
   ]
