bc20b62d826505c2f8179b277d0c7f59a78052fe
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-01-31 10:16:43 +0800

    navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="navbar">
+  
+  <b-nav pills>
+    <b-nav-item to="/login">Activities</b-nav-item>
+    <b-nav-item to="/">Registration</b-nav-item>
+    <b-nav-item ><b-form-input class="formInput"></b-form-input></b-nav-item>
+    <b-nav-item class="formDate">{{today | formatDate}}</b-nav-item>
+  </b-nav>
+      
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+name: 'Navbar',
+data(){
+  return{
+today: ""
+  }
+},
+methods:{
+   callFunction: function () {
+            var currentDate = new Date();
+             if(currentDate){
+               this.today = currentDate
+             }
+        }
+},
+mounted(){
+  this.callFunction();
+},
+filters:{
+formatDate: function(today) {
+      return moment(today, 'YYYY-MM-DD h:mm:ss a').format('DD/MM/YYYY h:mm a');
+       }
+          }
+}
+</script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #ffffff;
+  background: #1a1a1a;
+
+}
+.router-link-active:hover {
+  color:#ffffff;
+}
+.formDate{
+  margin-left:40%;
+
+}
+.formInput{
+  margin-left:400px;
+   height:25px;
+}
+.navbar{
+  background: #333333;
+}
+a.nav-link{
+  color: #ffffff;
+}
+ul.nav{
+  width:100%;
+}
+</style>
