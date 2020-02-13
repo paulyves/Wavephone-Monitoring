d14073ee0857f3b563b2a77da30de6087c48c89b
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-02-13 15:52:12 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,57 +1,68 @@
 <template>
-<div class="navBar">
-  <nav class="navbar navbar-expand">
-    <img src="../images/wavephone_logo_header-01.png" alt="" height="50px;" width="200px;" />
-    <div class="collapse navbar-collapse" style="border-left: solid 1px;
-  color: #ffffff;">
-      <ul class="navbar-nav ml-5">
-        <li><b-nav-item to="/Registration">Registration</b-nav-item></li>
-        <li><b-nav-item to="/Activities">Activities</b-nav-item></li>
-      </ul>
+  <div class="navBar">
+    <nav class="navbar navbar-expand">
+      <img
+        src="../images/wavephone_logo_header-01.png"
+        alt=""
+        height="50px;"
+        width="200px;"
+      />
+      <div
+        class="collapse navbar-collapse"
+        style="border-left: solid 1px;
+  color: #ffffff;"
+      >
+        <ul class="navbar-nav ml-5">
+          <li><b-nav-item to="/Registration">Registration</b-nav-item></li>
+          <li><b-nav-item to="/Activities">Activities</b-nav-item></li>
+        </ul>
 
-      <ul class="navbar-nav" style="margin-left:20%;">
+        <ul class="navbar-nav" style="margin-left:20%;">
+          <li>
+            <div>
+              <select
+                name="timeList"
+                form="listForm"
+                v-model="selectedTime"
+                class="form-control formInput"
+                value="2"
+                @click="selectInterval"
+              >
+                <option value="2">2s</option>
+                <option value="5">5s</option>
+                <option value="10">10s</option>
+                <option value="20">20s</option>
+              </select>
+            </div>
+          </li>
+          <li class="ml-3 ">
+            <b-form>
+              <b-input-group-append>
+                <b-form-input
+                  id="colorFil"
+                  v-model="search"
+                  type="search"
+                ></b-form-input>
+                <b-button class="material-icons" @click="btnSearch"
+                  >search</b-button
+                >
+              </b-input-group-append>
+            </b-form>
+          </li>
+          <li class="ml-5">
+            <p>Wave No.:</p>
+          </li>
+          <li class="ml-5">
+            <p class="ml-5">Host Name:</p>
+          </li>
+        </ul>
+      </div>
+      <ul>
         <li>
-          <div>
-            <select
-          name="timeList"
-          form="listForm"
-          :v-model="selectedTime"
-          class="form-control formInput"
-          value="2"
-        >
-          <option value="2">2s</option>
-          <option value="5">5s</option>
-          <option value="10">10s</option>
-          <option value="20">20s</option>
-        </select>
-          </div>
-          
+          <b-nav-item class="formDate">{{ today }}</b-nav-item>
         </li>
-        <li class="ml-3 ">
-          <b-form >
-      <b-input-group-append>
-        <b-form-input id="colorFil" v-model="search" type="search"></b-form-input>
-        <b-button class="material-icons"  @click="btnSearch">search</b-button>
-      </b-input-group-append>
-      </b-form>
-        </li>
-        <li class="ml-5">
-          <p>Wave No.:</p>
-        </li>
-        <li class="ml-5">
-          <p class="ml-5">Host Name:</p>
-        </li>
-        
       </ul>
-        
-    </div>
-    <ul>
-      <li>
-        <b-nav-item class="formDate">{{ today }}</b-nav-item>
-      </li>
-    </ul>
-  </nav>
-
+    </nav>
   </div>
 </template>
 
@@ -59,12 +70,13 @@
 import moment from "moment";
 export default {
   name: "Navbar",
-  props: ["selectedTime"],
+  // props: ["selectedTime"],
   data() {
     return {
       search: "",
       today: "",
-      searchWaveNum: ""
+      searchWaveNum: "",
+      selectedTime: ""
     };
   },
   methods: {
@@ -72,6 +84,9 @@ export default {
       this.$router.push({
         path: "/"
       });
+    },
+    selectInterval() {
+      this.$emit("pickTime", this.selectedTime);
     },
     btnSearch(event) {
       this.searchWaveNum = event.target.value;
@@ -102,37 +117,34 @@ export default {
 </script>
 
 <style scoped>
-.inputLi{
-width:100%;
-}
-ul.navbar-nav{
-  margin:0;
+ul.navbar-nav {
+  margin: 0;
 }
 .material-icons {
   border-radius: 2px;
   border: 1px solid black;
   border-left: none;
   background: #cccccc;
-
 }
 .material-icons:hover {
   background-color: white;
   border: 1px solid black;
   background: #cccccc;
-
 }
 
 #colorFil {
   border-radius: 1px;
   border: 1px solid black;
   border-right: none;
-  background: #cccccc; 
-
+  background: #cccccc;
 }
 .router-link-exact-active {
   color: #ffffff;
   background: #1a1a1a;
   border-radius: 5px;
+  height: 40px;
+  border: black solid 1px;
+  /* padding-top: 0; */
 }
 .router-link-active:hover {
   color: #ffffff;
@@ -140,7 +152,8 @@ ul.navbar-nav{
 p {
   color: #ffffff;
 }
-.formInput, .formInput:focus {
+.formInput,
+.formInput:focus {
   border: black solid 1px;
 }
 ul {
@@ -148,6 +161,7 @@ ul {
 }
 .navbar {
   background: #333333;
+  height: 50px;
 }
 a.nav-link {
   color: #ffffff;
@@ -155,12 +169,12 @@ a.nav-link {
 ul.nav {
   width: 100%;
 }
-.btn ,.btn:hover{
-  background: #cccccc; 
+.btn,
+.btn:hover {
+  background: #cccccc;
   color: #1a1a1a;
-
- }
- .material-icons{
+}
+.material-icons {
   color: #1a1a1a;
-} 
+}
 </style>
