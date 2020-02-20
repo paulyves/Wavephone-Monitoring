1e6ff1c9918a624f501602764ddd46ed0781e06f
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-02-20 15:42:47 +0800

    New views for navbar

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import router from './router'
 import store from './store';
 import BootstrapVue from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'material-icons/css/material-icons.css'
 import 'material-icons/iconfont/material-icons.css';
 
 
-Vue.config.productionTip = false
+
 Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,69 +1,161 @@
 <template>
   <div class="main">
-    <b-tabs pills v-model="btabs">
-      <b-tab disabled class="date-class">
-        <template v-slot:title>
-          <img
-            src="../images/wavephone_logo_header-01.png"
-            alt="Wavephone"
-            width="180px;"
-          />
-        </template>
-      </b-tab>
+    <b-tabs pills class="">
+      <template v-slot:tabs-start>
+        <!-- <div style="border-right: #ffffff solid 1px; border-height:5px; "> -->
+        <img
+          src="../images/wavephone_logo_header-01.png"
+          alt="Wavephone"
+          height="40px;"
+          width="200px;"
+        />
+        <!-- </div> -->
+      </template>
 
-      <b-tab
-        title="Registration"
-        class="nav-link mr-5"
-        :title-link-class="linkClass(1)"
-      >
-        <GridView
-      /></b-tab>
-      <b-tab
-        title="Activities"
-        class="nav-link mr-5"
-        :title-link-class="linkClass(2)"
-      >
-        <Activities
-      /></b-tab>
-      <b-tab align="right" :title="today" :class="date(today)" disabled></b-tab>
+      
+      <b-tab title="Registration" active>
+        <Registration
+        />
+      </b-tab>
+      <b-tab title="Activities">
+        <Activities />
+      </b-tab>
+     <template v-slot:tabs-end>
+
+
+        <b-nav-item class="select-form">
+          <select
+            name="timeList"
+            form="listForm"
+            class="form-control formInput"
+            @click="selectInterval"
+            v-model="timeInterval"
+          >
+            <option value="2">2s</option>
+            <option value="5">5s</option>
+            <option value="10">10s</option>
+            <option value="20">20s</option>
+          </select>
+        </b-nav-item>
+
+
+        <b-nav-item class="search-form">
+          <b-form>
+            <b-input-group-append>
+              <b-form-input
+                id="colorFil"
+                v-model="search"
+                type="search"
+              ></b-form-input>
+              <b-button class="material-icons" @click="btnSearch"
+                >search</b-button
+              >
+            </b-input-group-append>
+          </b-form>
+        </b-nav-item>
+        <b-nav-item class="formDate ">{{ today }}</b-nav-item>
+      </template>
     </b-tabs>
+   
   </div>
 </template>
 
 <script>
-import GridView from "./GridView.vue";
+import { mapActions, mapGetters } from "vuex";
+import Registration from "./GridView.vue";
 import Activities from "./Activities.vue";
 import moment from "moment";
 export default {
   name: "Main",
+  // props: ["value"],
   components: {
     Activities,
-    GridView
+    Registration
   },
   data() {
     return {
-      btabs: 0,
-      today: ""
+      search: "",
+      today: "",
+      searchWaveNum: "",
+      timeInterval: "2"
     };
   },
 
   mounted() {
+    this.myData();
     this.dateTime();
     this.interval = setInterval(this.dateTime, 1000);
   },
+  computed:{
+    ...mapGetters()
+  },
   methods: {
-    selectInterval(event) {
-      this.$emit("pickTime", event.target.value);
+    ...mapActions(["displaySample"]),
+  btnSearch() {
+    let numWave = this.search;
+      console.log(numWave)
+     
     },
-    btnSearch(event) {
-      this.searchWaveNum = event.target.value;
-      this.$emit("waveSearch", this.search);
-      this.search = "";
+   myData() {
+      this.displaySample().then(response => {
+        // console.log(response)
+
+        for (let wave in response) {
+          let extensions = response[wave];
+          let cell = document.getElementsByClassName(wave)[0];
+          let regExt = extensions.registered_extension.length;
+          let regDock = extensions.dock_registered;
+
+          if (regDock == true) {
+            cell.classList.remove("unRegistered", "unUsed");
+            cell.classList.add("isRegistered");
+
+            if (regExt != []) {
+              cell.innerHTML = regExt;
+            } else {
+              cell.innerHTML = "";
+            }
+          } else {
+            cell.classList.remove("isRegistered", "unUsed");
+            cell.classList.add("unRegistered");
+            if (regDock == false) {
+              if (regExt != []) {
+                cell.innerHTML = regExt;
+              } else {
+                cell.innerHTML = "";
+              }
+            }
+          }
+        }
+      });
     },
-    date(today) {
-      if (today) {
-        return ["text-light"];
+    logOutbtn() {
+      this.$router.push({
+        path: "/"
+      });
+    },
+    selectInterval() {
+      if (this.timeInterval == 2) {
+        clearInterval(this.timeData);
+        this.timeData = setInterval(this.myData, 2000);
+        console.log(this.timeInterval);
       }
+      if (this.timeInterval == 5) {
+        clearInterval(this.timeData);
+        this.timeData = setInterval(this.myData, 5000);
+        console.log(this.timeInterval);
+      } else if (this.timeInterval == 10) {
+        clearInterval(this.timeData);
+        this.timeData = setInterval(this.myData, 10000);
+        console.log(this.timeInterval);
+      } else if (this.timeInterval == 20) {
+        clearInterval(this.timeData);
+        this.timeData = setInterval(this.myData, 20000);
+        console.log(this.timeInterval);
+      } else if (this.timeInterval == 0) {
+        clearInterval(this.timeInterval);
+      }
+      console.log(this.value)
     },
     dateTime() {
       let currentDate = new Date();
@@ -71,60 +163,91 @@ export default {
         this.today = currentDate;
         this.today = moment(this.today, "M.DD.YYYY hh:mm:ss").format("L LT");
       }
-    },
-    linkClass(idx) {
-      let newTab = document.getElementsByClassName("active")[idx];
-      if (this.btabs === idx) {
-        console.log(idx, newTab);
-        return ["bg-dark", "text-light"];
-      }
+    }
+  },
+  created() {
+    if (this.timeInterval == 2) {
+      clearInterval(this.timeData);
+      this.timeData = setInterval(this.myData, 2000);
+      console.log(this.timeInterval);
+    }
+    if (this.timeInterval == 5) {
+      clearInterval(this.timeData);
+      this.timeData = setInterval(this.myData, 5000);
+      console.log(this.timeInterval);
+    } else if (this.timeInterval == 10) {
+      clearInterval(this.timeData);
+      this.timeData = setInterval(this.myData, 10000);
+      console.log(this.timeInterval);
+    } else if (this.timeInterval == 20) {
+      clearInterval(this.timeData);
+      this.timeData = setInterval(this.myData, 20000);
+      console.log(this.timeInterval);
+    } else if (this.timeInterval == 0) {
+      clearInterval(this.timeInterval);
     }
   }
 };
 </script>
 
 <style>
+.select-form{
+  margin-left: 18% !important;
+}
+.search-form{
+  margin-right: 30% !important;
+}
+.material-icons {
+  border-radius: 2px !important;
+  border: 1px solid black !important;
+  border-left: none !important;
+  background: #cccccc !important;
+  color: #1a1a1a !important;
+  height: 30px !important;
+  padding:0 !important;
+}
+.material-icons:hover {
+  background-color: white;
+  border: 1px solid black;
+  background: #cccccc !important;
+}
+
+#colorFil, #colorFil:focus {
+  border-radius: 1px;
+  border: 1px solid black;
+  border-right: none;
+  background: #cccccc;
+  height: 30px !important;
+}
+.formInput,
+.formInput:focus {
+  border: black solid 1px;
+  height: 30px !important;
+  padding:2px !important;
+}
+.nav-link.active {
+  background-color: #1a1a1a !important;
+}
 .nav-link.disabled {
-  color: #ffffff;
+  color: #ffffff !important;
 }
-.dateClass {
-  background-color: none;
-}
-.tabs.foobar .nav .nav-item.active {
-  /* custom styles here */
-  color: aqua;
-}
+
 .nav,
-.nav-pills{
+.nav-pills {
   background: #333333;
-  height:45px;
-}
-.tab-content > .active {
-  padding: 0;
-}
-.dateClass {
-  margin-left: 50%;
+  height: 43px;
 }
 .nav-link,
-.nav-link:hover , .nav-link.active:hover{
+.nav-link:hover,
+.nav-link.active:hover {
   color: #ffffff;
 }
-/* .nav-item {
+.nav-item {
   color: #333333;
-  margin-left:0px;
+  margin-left: 0px;
 }
-a{
+a {
   background-color: none;
+  margin-top: 0;
 }
-
-
-
-
-
-li.nav-item {
-  height: 60px;
-}
-a.router-link {
-  margin-top: 5px;
-} */
 </style>
