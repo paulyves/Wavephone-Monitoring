d4386f69cabbbd1b874ae6b85193013bb2b0dd5d
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-03-11 15:55:36 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,38 +1,45 @@
-import Vue from 'vue'
+import Vue from "vue";
 //import Home from './views/Home.vue'
-import Login from './views/login.vue'
-import GridView from './views/GridView.vue'
-import VueRouter from 'vue-router'
-import Activities from './views/Activities.vue'
-import Main from './views/Main.vue'
+import Login from "./views/login.vue";
+import GridView from "./views/GridView.vue";
+import VueRouter from "vue-router";
+import Activities from "./views/Activities.vue";
+import Main from "./views/Main.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 export default new VueRouter({
   mode: "history",
   routes: [
     {
-      path:'/',
-      name:'login',
+      path: "/",
+      name: "login",
       component: Login
     },
     {
-      path: '/Main',
-      name: 'main',
-      component: Main
+      path: "/Main",
+      name: "main",
+      component: Main,
+      meta: {
+        requiresAuth: true
+      }
     },
 
     {
-      path: '/Registration',
-      name: 'gridView',
-      component: GridView
+      path: "/Registration",
+      name: "gridView",
+      component: GridView,
+      meta: {
+        requiresAuth: true
+      }
     },
 
     {
-      path: '/Activities',
-      name: 'Activities',
-      component: Activities
+      path: "/Activities",
+      name: "Activities",
+      component: Activities,
+      meta: {
+        requiresAuth: true
+      }
     }
-    
   ]
-  
 });
