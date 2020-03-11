b931f82d55c0ce00e6af36b87483c70f851dfc75
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-03-11 15:55:56 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import Registered from './modules/registered';
 import ChartsData from './modules/chartsdata';
-
+import Data from './modules/data';
 Vue.use(Vuex);
 export default new Vuex.Store({
     modules:{
         Registered,
-        ChartsData
+        ChartsData,
+        Data
     }
 })
