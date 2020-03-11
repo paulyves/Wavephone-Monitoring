5151000b844bb9c8c96043c22e0fe4c1d884a4fc
JMillagracia-19 <54161594+JMillagracia-19@users.noreply.github.com>
2020-03-11 15:57:39 +0800

    Added a condition for not accessing the page when the user is not logged in.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store';
-import BootstrapVue from 'bootstrap-vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'material-icons/css/material-icons.css'
-import 'material-icons/iconfont/material-icons.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "material-icons/css/material-icons.css";
+import "material-icons/iconfont/material-icons.css";
 
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
-
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
-
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    /**
+     * condition where it will find routes that has a meta.requiresAuth
+     */
+    if (!store.getters.isLoggedIn) {
+      /**
+       * @param isLoggedIn = it's a getter where we store the token, if the token is null
+       * the user will be directed in the login page and if not null the user will be directed to the Home page.
+       */
+      next({
+        name: "login"
+      });
+    }
+    next();
+  } else {
+    next();
+  }
+});
 new Vue({
   render: h => h(App),
-router,
-store
-}).$mount('#app')
+  router,
+  store
+}).$mount("#app");
